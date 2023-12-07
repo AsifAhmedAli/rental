618c9059e0393d0cd3c86554e34cfc6bec1ac5a6
AsifAhmedAli <asif@mexil.it>
2023-12-07 13:41:31 +0500

    test

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,7 +2,7 @@ var request = require("request");
 // Configure Cloudinary
 
 const test = (req, res) => {
-  console.log("test");
+  // console.log("test");
   // console.log(req.body);
   var reservation_data = req.body.data;
   var user_data_from_hqrentals;
@@ -53,9 +53,10 @@ const test = (req, res) => {
           contact_data_from_freshworks.contacts.forEach((element) => {
             //
             if (element.email == user_data_from_hqrentals.contact.email) {
+              console.log("test");
               var options = {
                 method: "GET",
-                url: "https://api-america-3.us5.hqrentals.app/api-america-3/car-rental/reservations/additional-charges?pick_up_date=$req.body.data.pick_up_date&pick_up_time=req.body.data.pick_up_time&return_date=req.body.data.return_date&return_time=req.body.data.return_time&pick_up_location=req.body.data.pick_up_location_id&return_location=req.body.data.return_location_id&brand_id=req.body.data.brand_id&vehicle_class_id=req.body.data.vehicle_class_id",
+                url: `https://api-america-3.us5.hqrentals.app/api-america-3/car-rental/reservations/additional-charges?pick_up_date=${req.body.data.pick_up_date}&pick_up_time=${req.body.data.pick_up_time}&return_date=${req.body.data.return_date}&return_time=${req.body.data.return_time}&pick_up_location=${req.body.data.pick_up_location_id}&return_location=${req.body.data.return_location_id}&brand_id=${req.body.data.brand_id}&vehicle_class_id=${req.body.data.vehicle_class_id}`,
                 headers: {
                   Authorization:
                     "Basic MlVNUGQwNW1BVDZkZWczZExUaERIR0tFTGs5YWVzNE80bXdQcFVNNzFPSjI4MkNlRWg6aHc5aXRzc2lzTEVJb2pVU2ZvMlRtYVEwVWUyeTJRT1RzRG9rcTh3RVZpSXBkSUxmSFQ=",
